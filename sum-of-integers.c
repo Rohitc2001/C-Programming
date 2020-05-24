a873199ad86aab9cb00e3efc244b3659106f523d
Rohit Chaudhary <63979426+Rohitc2001@users.noreply.github.com>
2020-05-24 17:13:28 +0530

    Create sum-of-integers.c

---
+++ sum-of-integers.c
@@ -0,0 +1,16 @@
+                                // program to find sum of two integers 
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a = 5 , b = 5 ;
+	
+	printf("SUM OF a and b is : %d",a + b);
+	
+	getch();
+}
+
+/* Output
+ SUM OF a and b is : 10
+*/                               
