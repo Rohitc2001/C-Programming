dc2382900226238cf286e17e1ef408dd9df3798a
Rohit Chaudhary <63979426+Rohitc2001@users.noreply.github.com>
2020-06-28 10:13:18 +0530

    Add files via upload

---
+++ Sum-of-odd-num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+// Enter the 5 number from user and print the sum of odd number .
+// IDE used -> DEV C++
+
+int main()
+{
+	int a[4] ,i , sum=0 ;
+	
+	printf("Enter the numbers: \n");
+	
+	for(i=0 ; i<5 ; i++)
+	{
+		scanf("%d",&a[i]);
+		
+		if(a[i]%2!=0)
+		{
+			sum = sum + a[i];
+		}
+	}
+	printf("sum of odd numbers :%d ",sum);
+}
